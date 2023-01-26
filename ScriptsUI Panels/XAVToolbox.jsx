7cd81c2d1ea5a58fb9401365745244942b6e1c75
Racmanadan <dan@modeselect.net>
2023-01-26 12:15:19 -0800

    filepath Patch

--- ScriptsUI Panels/XAVToolbox.jsx
+++ ScriptsUI Panels/XAVToolbox.jsx
@@ -217,8 +217,11 @@
             patchPanel = mainToolBoxPanel.add("panel", undefined, "");
             patchPanel.margins = [4, 4, 4, 4];
             patchPanelgrp = patchPanel.add("group", undefined, "");
-            patchBTN = patchPanelgrp.add("button", undefined, "PATCH");
+
+            patchBTN = patchPanelgrp.add("button", undefined, "PATCH BUTTON", [100,100,200,200]);
+            patchBTN.text = "PATCH";
             patchBTN.size = [400, 25];
+            // patchBTN.graphics.backgroundColor = patchBTN.graphics.newBrush(patchBTN.graphics.BrushType.SOLID_COLOR, [1,0,0]);
             
             patchBTN.onClick = function(){
                 patchPaths();
@@ -4424,8 +4427,51 @@ function removeText(s){
 
 ////PATCH FUNCTION /////////
 
+    function replaceFilePath(item, newPath) {
+        if (item == null) {
+            return;
+        }
+        var currentPath = item.file.fsName;
+        var pathfix = decodeURI(newPath);
+        if (currentPath === pathfix) {
+            return;
+        }
+        item.replace(new File(pathfix));
+    }
+
     function patchPaths(){
-        alert("Files Patched!");
+        var filesInProject = [];
+        var filesToFixInProject = [];
+        var filesToCheck = [];
+        var fileList = new Folder(scriptPath + "/XAVToolbox_Assets/ImageResources").getFiles();
+        
+        for (var i = 0; i < fileList.length; i++) {
+            filesToCheck.push(fileList[i]);
+        }
+
+        for (var i = 0; i <= filesToCheck.length - 1; i++) {
+            var currentFile = filesToCheck[i];
+            var fileToFix = [];
+            var fileExists = false;
+            var currFilename = "";
+            for (var j = 1; j <= app.project.numItems; j++) {
+                if (app.project.item(j).name === currentFile.name) {
+                    fileToFix = app.project.item(j);
+                    fileExists = true;
+                    break;
+                }
+            }
+            if (fileExists) {
+                filesInProject.push(currentFile);
+                filesToFixInProject.push(fileToFix);
+            }
+        }
+
+        for (var i = 0; i <= filesInProject.length - 1; i++) {
+            replaceFilePath(filesToFixInProject[i], filesInProject[i]);
+        }
+
+        alert("Successfully patched!");
     }
 
 ////BUILD UI FUNCTION///////
